afedb3c7b46a04ec5e84746c12f259af822cff33
Andrey Okhotnikov <okhotnikovav@melonfashion.com>
2021-09-15 21:05:51 +0300

    fix: remove yarn.lock

--- database.js
+++ database.js
@@ -1,12 +1,12 @@
-const mysql = require('mysql')
+// const mysql = require('mysql')
 
-var dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'befree',
-}
+// var dbConfig = {
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root',
+//   database: 'befree',
+// }
 
-const connection = mysql.createConnection(dbConfig)
+// const connection = mysql.createConnection(dbConfig)
 
-module.exports = { connection }
+// module.exports = { connection }
